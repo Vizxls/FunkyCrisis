f41b1d454bbbb98b6a0cad8ccdacf41dea233e0c
TGH <tgh@Virus.local>
2025-08-12 17:33:11 +1200

    Initial commit

---
+++ assets/shared/stages/notscary.lua
@@ -0,0 +1,8 @@
+function onCreate()
+
+	makeLuaSprite('notscary', 'notscary', -500, 0);
+	setLuaSpriteScrollFactor('notscary', 0.1, 1);
+	scaleObject('notscary', 1, 1)
+
+	addLuaSprite('notscary', false)
+end
